36c9da7c9c0951b50e4483a88535b827e74fc152
ERAY <jameslebron23@hotmail.com.tw>
2020-12-02 13:47:12 +0800

    try to add calender

--- CalendarBudget/ViewController.swift
+++ CalendarBudget/ViewController.swift
@@ -6,12 +6,128 @@
 //
 
 import UIKit
+import HorizonCalendar
+
+struct DayLabel: CalendarItemViewRepresentable {
+
+  /// Properties that are set once when we initialize the view.
+  struct InvariantViewProperties: Hashable {
+    let font: UIFont
+    let textColor: UIColor
+    let backgroundColor: UIColor
+  }
+
+  //Properties that will vary depending on the particular date being displayed.
+  struct ViewModel: Equatable {
+    let day: Day
+  }
+
+  static func makeView(
+    withInvariantViewProperties invariantViewProperties: InvariantViewProperties)
+    -> UILabel
+  {
+    let label = UILabel()
+
+    label.backgroundColor = invariantViewProperties.backgroundColor
+    label.font = invariantViewProperties.font
+    label.textColor = invariantViewProperties.textColor
+
+    label.textAlignment = .center
+    label.clipsToBounds = true
+    label.layer.cornerRadius = 12
+    
+    return label
+  }
+
+  static func setViewModel(_ viewModel: ViewModel, on view: UILabel) {
+    view.text = "\(viewModel.day)"
+//    let dayText = "\(viewModel.day)"
+  }
+
+}
+final class DayRangeIndicatorView: UIView {
+
+  private let indicatorColor: UIColor
+
+  init(indicatorColor: UIColor) {
+    self.indicatorColor = indicatorColor
+    super.init(frame: frame)
+    backgroundColor = .clear
+  }
+
+  required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+
+  var framesOfDaysToHighlight = [CGRect]() {
+    didSet {
+      guard framesOfDaysToHighlight != oldValue else { return }
+      setNeedsDisplay()
+    }
+  }
+
+  override func draw(_ rect: CGRect) {
+    let context = UIGraphicsGetCurrentContext()
+    context?.setFillColor(indicatorColor.cgColor)
+
+    // Get frames of day rows in the range
+    var dayRowFrames = [CGRect]()
+    var currentDayRowMinY: CGFloat?
+    for dayFrame in framesOfDaysToHighlight {
+      if dayFrame.minY != currentDayRowMinY {
+        currentDayRowMinY = dayFrame.minY
+        dayRowFrames.append(dayFrame)
+      } else {
+        let lastIndex = dayRowFrames.count - 1
+        dayRowFrames[lastIndex] = dayRowFrames[lastIndex].union(dayFrame)
+      }
+    }
+
+    // Draw rounded rectangles for each day row
+    for dayRowFrame in dayRowFrames {
+      let roundedRectanglePath = UIBezierPath(roundedRect: dayRowFrame, cornerRadius: 12)
+      context?.addPath(roundedRectanglePath.cgPath)
+      context?.fillPath()
+    }
+  }
+
+}
+
+
 
 class ViewController: UIViewController {
+    
+    private func makeContent() -> CalendarViewContent {
+      let calendar = Calendar.current
 
+        let startDate = calendar.date(from: DateComponents(year: 2020, month: 01, day: 01))!
+        let endDate = calendar.date(from: DateComponents(year: 2021, month: 12, day: 31))!
+        let lowerDate = calendar.date(from: DateComponents(year: 2020, month: 01, day: 20))!
+        let upperDate = calendar.date(from: DateComponents(year: 2020, month: 02, day: 07))!
+        let dateRangeToHighlight = lowerDate...upperDate
+
+      return CalendarViewContent(
+        calendar: calendar,
+        visibleDateRange: startDate...endDate,
+        monthsLayout: .vertical(options: VerticalMonthsLayoutOptions()))
+//        monthsLayout: .horizontal(monthWidth: 300)
+        .withDayRangeItemModelProvider(for: [dateRangeToHighlight]) { dayRangeLayoutContext in
+              CalendarItemModel<DayRangeIndicatorView>(
+                invariantViewProperties: .init(indicatorColor: UIColor.blue.withAlphaComponent(0.15)),
+                viewModel: .init(framesOfDaysToHighlight: dayRangeLayoutContext.daysAndFrames.map { $0.frame }))
+            }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let calendarView = CalendarView(initialContent: makeContent())
+        view.addSubview(calendarView)
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+          calendarView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+          calendarView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+          calendarView.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+          calendarView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+        ])
     }
 
 
